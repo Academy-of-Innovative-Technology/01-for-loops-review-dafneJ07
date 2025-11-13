26f5409c3df23b3403f0b295c35157ddab1c7f2f
Dafne <dafnej@nycstudents.net>
2025-11-13 13:54:47 -0500

    hello

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,24 @@
+let runButton = document.querySelector('#pg-run');
+let resetButton = document.querySelector('#pg-reset');
+let resultRow = document.querySelector('#pg-row');
 
-// CREATE EVENT LISTENERS
+function runCountingLoop() {
+    resultRow.innerHTML = '';
+    
+ 
+    let start = Number(document.querySelector('#pg-start').value);
+    let end = Number(document.querySelector('#pg-end').value);
+    let step = Number(document.querySelector('#pg-step').value);
+    
+ 
+    for (let i = start; (step > 0) ? (i <= end) : (i >= end); i += step) {
+        resultRow.insertAdjacentHTML('beforeend', `<span class="chip">${i}</span> `);
+    }
+}
 
+function resetResults() {
+    resultRow.innerHTML = '';
+}
 
-
-
-
-
-
-
-
-
-
-
-// CREATE FUNCTIONS W/ LOOPS
+runButton.addEventListener('click', runCountingLoop);
+resetButton.addEventListener('click', resetResults);
